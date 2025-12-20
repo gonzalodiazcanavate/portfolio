3a78ca145c3222020a3ff1300fca8e6d8a57fc94
gonzalodiazcanavate <gonzalodiazca@gmail.com>
2025-12-20 01:25:52 +0100

    feat: Index añadido.

---
+++ frontend/src/pages/Index.tsx
@@ -0,0 +1,18 @@
+const Index = () => {
+  return (
+    <div className="min-h-screen bg-background">
+      {/* <Navbar /> */}
+      <main>
+        { /*
+        <Hero />
+        <About />
+        <Skills />
+        <Projects />
+        <Contact />
+        */ }
+      </main>
+    </div>
+  );
+};
+
+export default Index;
