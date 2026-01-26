51d9e3d77c725cc7e66f3e379c5b30e77951af44
gonzalodiazcanavate <gonzalodiazca@gmail.com>
2026-01-26 16:06:03 +0100

    feat: Curriculum actualizado.

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -62,7 +62,7 @@ const Hero = () => {
               </Button>
             </a>
             
-            <a target="_blank" href="/Curriculum-V3.pdf">
+            <a target="_blank" href="/Curriculum-V5.pdf">
               <Button size="lg" variant="outline" className="font-mono border-primary/50 hover:bg-primary/10 cursor-pointer">
                 {t.hero.downloadCV}
               </Button>
